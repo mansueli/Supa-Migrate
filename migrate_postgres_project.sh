3f0170d69ad6f0118f2827d6336d73f6f5736c99
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2023-02-02 15:16:10 -0300

    Update migrate_postgres_project.sh

--- migrate_postgres_project.sh
+++ migrate_postgres_project.sh
@@ -14,17 +14,15 @@ case "$(uname)" in
   Darwin*) sedi=(-i "")
 esac
 
-PGSSLMODE=allow pg_dump postgres://"$POSTGRES_USERNAME":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":5432" \
-  --dbname="$POSTGRES_ORIGIN_DATABASE" \
+PGSSLMODE=allow pg_dump postgres://"$POSTGRES_USERNAME":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":5432/"$POSTGRES_DATABASE" \
   --clean \
   --if-exists \
   --quote-all-identifiers \
   --exclude-schema 'extensions|graphql|graphql_public|net|pgbouncer|pgsodium|pgsodium_masks|realtime|supabase_functions|storage|pg_*|information_schema' \
   --schema '*' > dump.sql
-#sed -i -e 's/;max_client_conn = 100/max_client_conn = 200/g' pgbouncer.ini
 sed "${sedi[@]}" -e's/^DROP SCHEMA IF EXISTS "storage";$/-- DROP SCHEMA IF EXISTS "storage";/' dump.sql
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "auth";$/-- CREATE SCHEMA "auth";/' dump.sql
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "storage";$/-- CREATE SCHEMA "storage";/' dump.sql
-sed "${sedi[@]}" -e 's/"$POSTGRES_ORIGIN_DATABASE"/postgres/g' dump.sql
-sed "${sedi[@]}" -e 's/"$POSTGRES_ORIGIN_USERNAME"/postgres/g' dump.sql
-# psql postgres://postgres:"$SUPA_PASSWORD"@"$SUPA_URL":6543/postgres --file dump.sql
+sed "${sedi[@]}" -e "s/'$POSTGRES_ORIGIN_DATABASE'/postgres/g" dump.sql
+sed "${sedi[@]}" -e "s/'$POSTGRES_ORIGIN_USERNAME'/postgres/g" dump.sql
+psql postgres://postgres:"$SUPA_PASSWORD"@"$SUPA_URL":6543/postgres --file dump.sql
