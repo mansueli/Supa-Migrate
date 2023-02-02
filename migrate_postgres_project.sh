215abdeb1a014227304923257a9862f4bf3297c0
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2023-02-02 14:53:09 -0300

    Update migrate_postgres_project.sh

--- migrate_postgres_project.sh
+++ migrate_postgres_project.sh
@@ -14,7 +14,7 @@ case "$(uname)" in
   Darwin*) sedi=(-i "")
 esac
 
-PGSSLMODE=allow pg_dump postgres://"$POSTGRES_USERNAME":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":5432/"$POSTGRES_ORIGIN_DATABASE" \
+PGSSLMODE=allow pg_dump postgres://"$POSTGRES_USERNAME":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":5432/"${POSTGRES_ORIGIN_DATABASE}" \
   --clean \
   --if-exists \
   --quote-all-identifiers \
