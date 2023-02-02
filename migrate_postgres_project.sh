be13c863bd4b72a6d5847ad2954697a7bb69d42b
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2023-02-02 15:02:35 -0300

    Update migrate_postgres_project.sh

--- migrate_postgres_project.sh
+++ migrate_postgres_project.sh
@@ -15,7 +15,7 @@ case "$(uname)" in
 esac
 
 PGSSLMODE=allow pg_dump postgres://"$POSTGRES_USERNAME":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":5432" \
-  --dbname=${POSTGRES_ORIGIN_DATABASE} \ 
+  --dbname="$POSTGRES_ORIGIN_DATABASE" \ 
   --clean \
   --if-exists \
   --quote-all-identifiers \
