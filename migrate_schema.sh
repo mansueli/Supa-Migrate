2c0fe4a512d8713f54a010f272d899f2e5e94bbd
Rodrigo Mansueli <rodrigo@mansueli.com>
2024-01-25 16:20:39 -0300

    Update migrate_schema.sh

--- migrate_schema.sh
+++ migrate_schema.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 #Edit here:
-OLD_DB_URL=db.old_project_ref.supabase.co
-NEW_DB_URL=db.new_project_ref.supabase.co
-OLD_DB_PASS=secret_password_here
-NEW_DB_PASS=secret_new_password_here
+OLD_SUPAVISOR_URL=postgres://postgres.oldproject:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:5432/postgres
+NEW_SUPAVISOR_URL=postgres://postgres.newproject:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:5432/postgres
+
 #Script:
 # Default case for Linux sed, just use "-i"
 sedi=(-i)
@@ -13,12 +12,12 @@ case "$(uname)" in
   Darwin*) sedi=(-i "")
 esac
 
-pg_dump postgres://postgres:"$OLD_DB_PASS"@"$OLD_DB_URL":6543/postgres \
+pg_dump "$OLD_SUPAVISOR_URL" \
   --clean \
   --if-exists \
-  --schema-only \
   --quote-all-identifiers \
   --exclude-table-data 'storage.objects' \
+  --schema-only
   --exclude-schema 'extensions|graphql|graphql_public|net|tiger|pgbouncer|vault|realtime|supabase_functions|storage|pg*|information_schema' \
   --schema '*' > dump.sql 
 
@@ -27,4 +26,4 @@ sed "${sedi[@]}" -e's/^DROP SCHEMA IF EXISTS "storage";$/-- DROP SCHEMA IF EXIST
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "auth";$/-- CREATE SCHEMA "auth";/' dump.sql
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "storage";$/-- CREATE SCHEMA "storage";/' dump.sql
 sed "${sedi[@]}" -e 's/^ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/-- ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/' dump.sql
-psql postgres://postgres:"$NEW_DB_PASS"@"$NEW_DB_URL":6543/postgres --file dump.sql
+psql "$NEW_SUPAVISOR_URL" --file dump.sql 
