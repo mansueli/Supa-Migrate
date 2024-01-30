e1f64fd1b111817ff1158746a9acc6d302587ff8
Rodrigo Mansueli <rodrigo@mansueli.com>
2024-01-30 10:52:32 -0300

    update-migration-script

--- migrate_project.sh
+++ migrate_project.sh
@@ -13,27 +13,25 @@ case "$(uname)" in
 esac
 
 pg_dump "$OLD_SUPAVISOR_URL" \
-  --clean \
   --if-exists \
+  --clean \
   --quote-all-identifiers \
-  --exclude-table-data 'storage.objects' \
-  --schema-only
+  --schema-only \
+  --no-owner --no-privileges \
   --exclude-schema 'extensions|graphql|graphql_public|net|tiger|pgbouncer|vault|realtime|supabase_functions|storage|pg*|information_schema' \
-  --schema '*' > dump.sql 
-  
+  --schema '*' > dump.sql
+
 pg_dump "$OLD_SUPAVISOR_URL" \
-  --clean \
-  --if-exists \
   --quote-all-identifiers \
-  --exclude-table-data 'storage.objects' \
-  --data-only
+  --no-owner --no-privileges \
+  --data-only \
   --exclude-schema 'extensions|graphql|graphql_public|net|tiger|pgbouncer|vault|realtime|supabase_functions|storage|pg*|information_schema' \
-  --schema '*' > data_dump.sql 
+  --schema '*' > data_dump.sql
 
 sed "${sedi[@]}" -e 's/^DROP SCHEMA IF EXISTS "auth";$/-- DROP SCHEMA IF EXISTS "auth";/' dump.sql
 sed "${sedi[@]}" -e's/^DROP SCHEMA IF EXISTS "storage";$/-- DROP SCHEMA IF EXISTS "storage";/' dump.sql
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "auth";$/-- CREATE SCHEMA "auth";/' dump.sql
 sed "${sedi[@]}" -e 's/^CREATE SCHEMA "storage";$/-- CREATE SCHEMA "storage";/' dump.sql
 sed "${sedi[@]}" -e 's/^ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/-- ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/' dump.sql
-psql "$NEW_SUPAVISOR_URL" --file dump.sql 
-psql "$NEW_SUPAVISOR_URL" --file data_dump.sql 
+psql "$NEW_SUPAVISOR_URL" --file dump.sql
+psql "$NEW_SUPAVISOR_URL" --file data_dump.sql
